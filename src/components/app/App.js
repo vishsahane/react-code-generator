5f61f584fbf50e8ea0bdf3ad4123ebd2df6f33f9
Vishwas Sahane <sahanevishwas@yahoo.com>
2020-11-15 15:45:08 +0530

    Initial Setup

---
+++ src/components/app/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Container from '@material-ui/core/Container'
+
+import logo from '../../logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Container className="conatiner">
+        Hello
+      </Container>
+    )
+  }
+}
+
+export default App
