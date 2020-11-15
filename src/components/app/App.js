fc618fc23a6f31934bde40b17de26eb5e9c6b331
Vishwas Sahane <sahanevishwas@yahoo.com>
2020-11-15 17:23:44 +0530

    Added sections

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,89 @@
-import React, { Component } from 'react'
+
+import React from 'react';
+
 import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
 
-import logo from '../../logo.svg'
-import './App.css'
+import { CONTROLS } from '../../constants/controls'
 
-class App extends Component {
-  render() {
-    return (
-      <Container className="conatiner">
-        Hello
-      </Container>
-    )
-  }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+  },
+}));
+
+console.log("CONTROLS", CONTROLS)
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Grid container spacing={3}>
+        <Grid item md={3}>
+          <Card >
+            <CardHeader title="Controls" />
+            <CardContent>
+              <List>
+                {CONTROLS.map(control =>
+                  <ListItem>
+                    <ListItemIcon>
+                      <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={control.label}
+                    />
+                  </ListItem>
+                )}
+              </List>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={6}>
+          <Card >
+            <CardHeader title="Editor" />
+            <CardContent>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item md={3}>
+          <Card >
+            <CardHeader title="Properties" />
+            <CardContent>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
-export default App
---
+++ src/constants/controls.js
@@ -0,0 +1,5 @@
+import LabelIcon from '@material-ui/icons/Label';
+export const CONTROLS = [
+    {label:'Button', name: 'Button', icon:LabelIcon},
+    {label:'Label', name: 'Label', icon:LabelIcon}
+]
